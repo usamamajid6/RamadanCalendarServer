d5d189bbe42ed849087772e15d1f74bab7989f7b
Usama Majid <usamamajid6@gmail.com>
2020-04-26 23:39:57 +0500

    make it better

--- index.js
+++ index.js
@@ -100,31 +100,31 @@ ${tableString}</body>
 
 app.use(cors());
 app.get("/", (req, res) => res.send("Hello World!"));
-// app.get("/getTimings/:city", (req, res) => {
-//   axios({
-//     method: "get",
-//     url: `https://muslimsalat.com/${req.params.city}/yearly/25-04-2020.json?key=${key}`,
-//   })
-//     .then((response) => {
-//       if (response.data.status_description === "Failed.") {
-//         res.status(204).json({
-//           response: response.data,
-//           status: "Error in Search String",
-//         });
-//       } else {
-//         res.status(200).json({
-//           response: response.data,
-//           status: "Success",
-//         });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(503).json({
-//         error,
-//       });
-//     });
-// });
+app.get("/getTimings/:city", (req, res) => {
+  axios({
+    method: "get",
+    url: `https://muslimsalat.com/${req.params.city}/yearly/25-04-2020.json?key=${key}`,
+  })
+    .then((response) => {
+      if (response.data.status_description === "Failed.") {
+        res.status(204).json({
+          response: response.data,
+          status: "Error in Search String",
+        });
+      } else {
+        res.status(200).json({
+          response: response.data,
+          status: "Success",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(503).json({
+        error,
+      });
+    });
+});
 
 app.get("/getImage/:city", (req, res) => {
   axios({
